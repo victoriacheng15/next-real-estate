a764550f744b91f246352a204a05dd39b3b6c37a
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-30 17:22:40 -0700

    feat: add ChakraProvider and nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,27 @@
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import Router from "next/router";
+import NProgress from "nprogress";
+import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	NProgress.configure({ showSpinner: false });
+
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start();
+  });
+
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done();
+  });
+
+	return (
+		<>
+			<ChakraProvider>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</ChakraProvider>
+		</>
+	);
 }
