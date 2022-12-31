cbdfb4ff3615040706dc341ccbc649c04c393bdd
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-30 17:21:22 -0700

    feat: add Layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import { ReactNode } from "react";
+import { Box, layout } from "@chakra-ui/react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+	return (
+		<>
+			<Head>
+				<title>Real Estate - Nextjs</title>
+				<meta
+					name="description"
+					content="show rentable or buyable properties on the market"
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Box maxWidth="1280px" m="auto">
+				<header>
+					<h1>navbar</h1>
+				</header>
+				<main>{children}</main>
+				<footer>
+					<h1>footer</h1>
+				</footer>
+			</Box>
+		</>
+	);
+};
+
+export default Layout;
