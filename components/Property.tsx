b1861711596fbe7e1d6cd71a9e6dd5edd8486cbc
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-31 21:46:35 -0700

    chore: update file path for PropertyProp

--- components/Property.tsx
+++ components/Property.tsx
@@ -5,7 +5,7 @@ import { FaBed, FaBath } from "react-icons/fa";
 import { BsGridFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
 import millify from "millify";
-import { PropertyProps } from "../types/Properties";
+import { PropertyProps } from "../types/properties";
 
 import DefaultImage from "../assets/images/house.jpg";
 
