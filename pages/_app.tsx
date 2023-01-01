ba0791bca7a04aaf2c7955a59d73eb731221206f
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-31 21:46:56 -0700

    fix: remove Head and nprogress link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from "next/app";
-import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 import Router from "next/router";
 import NProgress from "nprogress";
@@ -18,15 +17,6 @@ export default function App({ Component, pageProps }: AppProps) {
 
 	return (
 		<>
-			<Head>
-				<link
-					rel='stylesheet'
-					href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css'
-					integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=='
-					crossOrigin='anonymous'
-					referrerPolicy='no-referrer'
-				/>
-			</Head>
 			<ChakraProvider>
 				<Layout>
 					<Component {...pageProps} />
