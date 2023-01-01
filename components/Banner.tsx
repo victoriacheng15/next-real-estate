b3e0b12c571541b734d8e0d29d99515bf7ecb9e9
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-31 17:03:33 -0700

    chore: use Record type and adjust css properties

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,16 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
-type BannerProps = {
-	imageUrl: string;
-	purpose: string;
-	title1: string;
-	title2: string;
-	desc1: string;
-	desc2: string;
-	linkName: string;
-	buttonText: string;
-};
+type BannerProps = Record<string, string>;
 
 const Banner = ({
 	imageUrl,
@@ -23,7 +14,7 @@ const Banner = ({
 	linkName,
 	buttonText,
 }: BannerProps) => (
-	<Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+	<Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10" gap="8">
 		<Image src={imageUrl} width={500} height={300} alt="banner" />
 		<Box>
 			<Text color="gray.500" fontSize="sm" fontWeight="medium">
@@ -35,7 +26,7 @@ const Banner = ({
 			<Text fontSize="lg" paddingTop="3" paddingBottom="3" color="gray.700">
 				{desc1} <br /> {desc2}
 			</Text>
-			<Button fontSize="xl" bgColor="cyan" color="black">
+			<Button fontSize="xl" bg="blue.300" color="black">
 				<Link href={linkName}>{buttonText}</Link>
 			</Button>
 		</Box>
